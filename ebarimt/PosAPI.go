a104dedda473208c897ac81a6453c4f4affa82ee
munkh-altai <munkaltai@gmail.com>
2023-04-07 01:53:15 +0800

    so file path changed

--- ebarimt/PosAPI.go
+++ ebarimt/PosAPI.go
@@ -5,7 +5,7 @@ import "github.com/lambda-platform/ebarimt/posapi"
 var PosAPI *posapi.PosAPI
 
 func init() {
-	api, err := posapi.NewPosAPI("/home/khankhulgen/web/ebarimt/sofiles/mmk.so")
+	api, err := posapi.NewPosAPI("/home/ebarimtuser/app/libPosAPI.so")
 	if err != nil {
 		panic(err)
 	}
